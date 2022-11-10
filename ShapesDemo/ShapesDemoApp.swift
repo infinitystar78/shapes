a2261ff8292b6ab1bf6b5634ef4c9e5e690516cc
Melvin W <melvinhw@me.com>
2022-11-10 12:14:50 +0000

    Initial Commit

---
+++ ShapesDemo/ShapesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesDemoApp.swift
+//  ShapesDemo
+//
+//  Created by M W on 10/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShapesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
